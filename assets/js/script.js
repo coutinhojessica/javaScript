3f93424d66d5d7dade87434a6689704baebc19f2
Jéssica Coutinho <jessicamaiara.coutinho@gmail.com>
2022-09-11 14:58:53 -0300

    Modificações feitas devido a aula 5 de variáeis.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
+//4 Aula - Olá mundo!
 //alert("Olá Mundo!");
-console.log("Olá mundo 3.0");
+//console.log("Olá mundo 3.0");
+
+
+// Aula 5 - variáveis
+let idade = 26;
+let nome = "Jéssica";
+
+console.log("Meu nome é: " +nome);
+console.log("Minha idade é: " +idade);
+
+let cidade = "Santa Rita";
+let estado = "Paraíba";
+
+console.log("Jéssica moram em " +cidade)
+console.log("Santa Rita fica no estado da " +estado);
