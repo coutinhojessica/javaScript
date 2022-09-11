04f43814eae83fd8210914c9773671252688c472
Jéssica Coutinho <jessicamaiara.coutinho@gmail.com>
2022-09-11 15:21:23 -0300

    Modificações devido a aula 6 de tipos de dados.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 
 // Aula 5 - variáveis
-let idade = 26;
+/*let idade = 26;
 let nome = "Jéssica";
 
 console.log("Meu nome é: " +nome);
@@ -14,4 +14,13 @@ let cidade = "Santa Rita";
 let estado = "Paraíba";
 
 console.log("Jéssica moram em " +cidade)
-console.log("Santa Rita fica no estado da " +estado);
+console.log("Santa Rita fica no estado da " +estado);*/
+
+
+// 6 aula - Tipos de dados
+    /* String
+    Number
+    boolean - true or false
+    array ["descrição" , "Descrição" , "Descrição"]
+    object {}
+    typeof - para descobrir o tipo de dado*/
